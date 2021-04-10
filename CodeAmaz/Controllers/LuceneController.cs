302458ebb9a0dd39b11c1116ca7e2a918ac819b6
DanhSy <danhsy2212@gmail.com>
2021-04-10 10:45:51 +0700

    sy tao branch

---
+++ CodeAmaz/Controllers/LuceneController.cs
@@ -0,0 +1,34 @@
+﻿using Lucene.Net.Index;
+using Lucene.Net.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeAmaz.Controllers
+{
+    public class LuceneController : Controller
+    {
+        private static string _luceneDir=
+        Path.Combine(HttpContext.Current.Request.PhysicalApplicationPath, "lucene_index");
+        private static FSDirectory _directoryTemp;
+        private static FSDirectory _directory
+        {
+            get
+            {
+                if (_directoryTemp == null) _directoryTemp = FSDirectory.Open(new DirectoryInfo(_luceneDir));
+                if (IndexWriter.IsLocked(_directoryTemp)) IndexWriter.Unlock(_directoryTemp);
+                var lockFilePath = Path.Combine(_luceneDir, "write.lock");
+                if (File.Exists(lockFilePath)) File.Delete(lockFilePath);
+                return _directoryTemp;
+            }
+        }
+        //// GET: Lucene
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
---
+++ CodeAmaz/Controllers/SampleDataController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeAmaz.Controllers
+{
+    public class SampleDataController : Controller
+    {
+        public class SampleData
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+        }
+        public static class SampleDataRepository
+        {
+            public static SampleData Get(int id)
+            {
+                return GetAll().SingleOrDefault(x => x.Id.Equals(id));
+            }
+        }
+        public static List<SampleData> GetAll()
+        {
+            return new List<SampleData>
+            {
+                new SampleData { Id = 1, Name = "CHUDE", Description = "Chu de mon hoc" },
+                new SampleData { Id = 2, Name = "KHOAHOC", Description = "Khoa hoc" },
+            };
+        }
+        //    public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+    }
+}
